b0f3f9b1fe130c5bf0484a83b9cf55a2d66b09ef
Louie Antony <56921440+LouieAntony@users.noreply.github.com>
2023-01-16 06:29:06 +0530

    Update clusters.R

--- DS lab/clusters.R
+++ DS lab/clusters.R
@@ -7,15 +7,14 @@ library("ggfortify")
 summary(iris)
 head(iris)
 data <- select(iris, c(1:4))
-wssplot <- function(data, nc=15, seed=1234){
-  wss <- (nrow(data)-1)*sum(apply(data,2,var))
-  for (i in 2:nc){
-    set.seed(seed)
-    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
-  plot(1:nc, wss, type="b", xlab="Number of Clusters",
-       ylab="Within groups sum of squares")
-  wss
+
+wss <- (nrow(data)-1)*sum(apply(data,2,var))
+for (i in 2:15)
+{
+  wss[i] <- sum(kmeans(data, centers=i)$withinss)
 }
+plot(1:15, wss, type="b", xlab="Number of Clusters",ylab="Within groups sum of squares")
+
 kmean <- kmeans(data, 2)
 kmean$centers
 autoplot(kmean, data, frame = TRUE)
